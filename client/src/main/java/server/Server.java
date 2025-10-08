b4cd10a405d9bbcb29331615b0be66b17b2f5756
hawsmat <hawsmat17@gmail.com>
2025-10-08 15:00:05 -0600

    delete and register

--- client/src/main/java/server/Server.java
+++ client/src/main/java/server/Server.java
@@ -4,21 +4,25 @@ import io.javalin.*;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
+
+        server.delete("delete", ctx -> ctx.result("{}"));
+        server.post("user", ctx -> ctx.result("{ \"username\":\"joe\", \"authToken\":\"xyz\"}"));
+
 
         // Register your endpoints and exception handlers here.
 
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }
