d50f71933bc8989c1fd840fea28166371e1a44e2
hawsmat <hawsmat17@gmail.com>
2025-10-13 15:06:50 -0600

    Datamodels

--- client/src/main/java/server/Server.java
+++ client/src/main/java/server/Server.java
@@ -1,20 +1,36 @@
 package server;
 
+import Service.UserService;
+import com.google.gson.Gson;
+import datamodel.User;
 import io.javalin.*;
+import io.javalin.http.Context;
+import java.util.Map;
 
 public class Server {
 
     private final Javalin server;
+    private UserService userService = new UserService();
 
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
         server.delete("delete", ctx -> ctx.result("{}"));
-        server.post("user", ctx -> ctx.result("{ \"username\":\"joe\", \"authToken\":\"xyz\"}"));
+        server.post("user", ctx -> register(ctx));
 
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(ctx.body(), User.class);
+
+        var res = userService.register(req);
+
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
