fa6361812f78d4ea719edc7bd1f5983fe0335f2f
hawsmat <hawsmat17@gmail.com>
2025-12-05 00:01:08 -0700

    Code Quality

--- server/src/main/java/websocket/commands/UserGameCommand.java
+++ server/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,13 +1,5 @@
 package websocket.commands;
 
-import java.util.Objects;
-
-/**
- * Represents a command a user can send the server over a websocket
- * <p>
- * Note: You can add to this class, but you should not alter the existing
- * methods.
- */
 public class UserGameCommand {
     public enum CommandType {
         CONNECT,
--- server/src/main/java/websocket/messages/ServerMessage.java
+++ server/src/main/java/websocket/messages/ServerMessage.java
@@ -1,15 +1,5 @@
 package websocket.messages;
-
-import java.util.Objects;
-
-/**
- * Represents a Message the server can send through a WebSocket
- * <p>
- * Note: You can add to this class, but you should not alter the existing
- * methods.
- */
 public class ServerMessage {
-
     public enum ServerMessageType {
         LOAD_GAME,
         ERROR,
